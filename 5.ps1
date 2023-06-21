f655a400498c4049d25a7e4222b23b929e83bd75
rinquan861 <132658564+rinquan861@users.noreply.github.com>
2023-06-21 15:54:51 +0700

    Create 5.ps1

---
+++ 5.ps1
@@ -0,0 +1,98 @@
+net.exe start Audiosrv
+
+net.exe user RinneVPS Rinne@001 /add
+
+net localgroup administrators RinneVPS /add
+
+
+choco install ngrok -y
+
+ngrok authtoken 2MwTQ2vL3WGT7QSnd9QcBD3eE7D_S8T2mYhRs4KnCR7aQKmk
+
+Start-Process Powershell -ArgumentList '-Noexit -Command ".\ngrok\ngrok.exe tcp 3389"'
+
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
+sleep 43200
